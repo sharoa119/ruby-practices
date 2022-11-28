e4efc0395304d9f95b28d5415fee365fe3ed8759
sharoa119 <sau.rin1109@gmail.com>
2022-11-28 17:22:14 +0900

    修正しました。

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,52 +1,46 @@
 # frozen_string_literal: true
 
 score = ARGV[0]
-scores = score.split(',')
+all_scores = score.split(',')
 
 frames = Array.new(10) { [] }
-frame = 1
-scores.each do |s|
-  if frame == 10
-    frames[frame - 1] << if s == 'X'
-                           10
-                         else
-                           s.to_i
-                         end
+frame = 0
+PERFECT_SCORE = 10
+all_scores.each do |s|
+  if frame == 9 # 0から始まるので10フレーム目は９になる。
+    frames[frame] << (s == 'X' ? PERFECT_SCORE : s.to_i)
     next
   end
 
   if s == 'X'
-    frames[frame - 1] << 10
-    frames[frame - 1] << 0
+    frames[frame] << PERFECT_SCORE
+    frames[frame] << 0
     frame += 1
     next
   end
-  frames[frame - 1] << s.to_i
+  frames[frame] << s.to_i
 
-  if frames[frame - 1].size == 2
+  if frames[frame].size == 2
     frame += 1
     next
   end
 end
 
 point = 0
-frame = 1
-frames.each do |count|
-  point += count.sum if frame == 10
-
-  if frame != 10
-    point += if count[0] == 10 && frame == 9
-               10 + frames[frame][0] + frames[frame][1]
-             elsif count[0] == 10 && frames[frame][0] == 10 # ストライクかつ次もストライクだった場合
-               10 + frames[frame][0] + frames[frame + 1][0]
-             elsif count[0] == 10 # ストライク
-               10 + frames[frame][0] + frames[frame][1]
-             elsif count.sum == 10 # スペア
-               10 + frames[frame][0]
+frames.each.with_index(1) do |scores, i|
+  point += scores.sum if i == 10
+  if i != 10
+    point += if scores[0] == 10 && i == 9
+               10 + frames[i][0] + frames[i][1]
+             elsif scores[0] == 10 && frames[i][0] == 10 # ストライクかつ次もストライクだった場合
+               10 + frames[i][0] + frames[i + 1][0]
+             elsif scores[0] == 10 # ストライク
+               10 + frames[i][0] + frames[i][1]
+             elsif scores.sum == 10 # スペア
+               10 + frames[i][0]
              else
-               count.sum
+               scores.sum
              end
   end
-  frame += 1
 end
 puts point
