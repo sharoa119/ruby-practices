9f69e8c38ac592ab5e3e068b8cd88734edf17770
sharoa119 <sau.rin1109@gmail.com>
2024-10-24 11:53:29 +0900

    Frameクラスが完成

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -6,22 +6,22 @@ class Frame
   attr_reader :shots
 
   def initialize
-    @shots = [] # shotsに空の配列を初期値として渡す、なぜならframeが呼び出されるたびクリアになっているべきだから。
+    @shots = []
   end
 
-  def add_shot(pin) # 投げた時のpinをshotsに追加していく。
+  def add_shot(pin)
     @shots << Shot.new(pin)
   end
 
-  def strike? # ストライクかの確認メソッド
-    @shots[0].pins == 10 # 1回目で投げた時にpinsの数値が10だったら。（[0]はインデックスで1回目。）
+  def strike?
+    @shots[0].pins == 10
   end
 
-  def spare? # スペアかの確認メソッド
-    @shots.size == 2 && @shots.sum(&:pins) == 10 # shotsが2回分あって、尚且つ、その2つの数値を足した数が10だったら。
+  def spare?
+    @shots.size == 2 && @shots.sum(&:pins) == 10
   end
 
   def score
-    @shots.sum(&:pins) # pinsを足した数がスコア。
+    @shots.sum(&:pins)
   end
 end
