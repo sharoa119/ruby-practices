f859b1edf961df698186f6d2457368517ee067eb
sharoa119 <sau.rin1109@gmail.com>
2024-10-24 11:53:00 +0900

    Shotクラスが完成

--- 07.bowling_object/shot.rb
+++ 07.bowling_object/shot.rb
@@ -5,7 +5,7 @@ class Shot
 
   attr_reader :pins
 
-  def initialize(pin) # 引数pinsを渡す
-    @pins = pins == 'X' ? PERFECT_SCORE : pin.to_i # pinsがXだったら10を代入。それ以外だったら、整数に変換して代入。
+  def initialize(pin)
+    @pins = pin == 'X' ? PERFECT_SCORE : pin.to_i
   end
 end
