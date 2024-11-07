a6cb1a4c177ff56905635ae11d46eba65f38e359
sharoa119 <sau.rin1109@gmail.com>
2024-11-07 16:39:16 +0900

    必要なファイルのrequireだけ書いておいた

--- 08.ls_object/ls_object.rb
+++ 08.ls_object/ls_object.rb
@@ -1,2 +1,9 @@
 # frozen_string_literal: true
 
+# 必要なファイルをrequire
+require_relative 'path_collection'
+require_relative 'path_display'
+require_relative 'file_detail_display'
+require_relative 'detailed_file'
+
+params = ARGV.getopts('alr')
