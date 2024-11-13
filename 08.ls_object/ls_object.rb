f0a9e487056b9ced7d5331ebb9f33f2fceb95105
sharoa119 <sau.rin1109@gmail.com>
2024-11-13 11:30:42 +0900

    shebangをつけた

--- 08.ls_object/ls_object.rb
+++ 08.ls_object/ls_object.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 # frozen_string_literal: true
 
 # 必要なファイルをrequire
@@ -7,3 +8,17 @@ require_relative 'file_detail_display'
 require_relative 'detailed_file'
 
 params = ARGV.getopts('alr')
+
+# ファイルのリストを取得
+path_collection = PathCollection.new(params)
+paths = path_collection.paths
+
+# 出力方法を選択
+if params['l']
+  file_details = paths.map { |path| DetailedFile.new(path) }
+  file_detail_display = FileDetailDisplay.new(file_details)
+  file_detail_display.show
+else
+  path_display = PathDisplay.new(paths)
+  path_display.show
+end
