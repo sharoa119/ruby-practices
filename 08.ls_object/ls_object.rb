0f5e3bba5ea42afc1f9516e4f1a03ddb232693af
sharoa119 <sau.rin1109@gmail.com>
2024-11-14 12:12:49 +0900

    メインとなるファイルを考えた。まだ途中

--- 08.ls_object/ls_object.rb
+++ 08.ls_object/ls_object.rb
@@ -7,18 +7,21 @@ require_relative 'path_display'
 require_relative 'file_detail_display'
 require_relative 'detailed_file'
 
+# コマンドライン引数を取得
 params = ARGV.getopts('alr')
 
 # ファイルのリストを取得
-path_collection = PathCollection.new(params)
+path_collection = PathCollection.new(dotmatch: params['a'], reverse: params['r'])
 paths = path_collection.paths
 
 # 出力方法を選択
 if params['l']
+  # 詳細表示用のインスタンスを生成し、表示
   file_details = paths.map { |path| DetailedFile.new(path) }
   file_detail_display = FileDetailDisplay.new(file_details)
   file_detail_display.show
 else
+  # 列表示用のインスタンスを生成し、表示
   path_display = PathDisplay.new(paths)
   path_display.show
 end
