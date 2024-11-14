3d4c08ef5979e22c9e54e1ebe884cb671049e09b
sharoa119 <sau.rin1109@gmail.com>
2024-11-14 12:14:34 +0900

    ファイルの一覧を取得し、オプションに応じて並べ替えてみる。まだ途中

--- 08.ls_object/path_collection.rb
+++ 08.ls_object/path_collection.rb
@@ -1,4 +1,18 @@
 # frozen_string_literal: true
 
 class PathCollection
+  def initialize(dotmatch: false, reverse: false)
+    @dotmatch = dotmatch # 隠しファイルを含むかどうか
+    @reverse = reverse # ファイルリストを逆順にするかどうか
+  end
+
+  def paths
+    files = if @dotmatch # 隠しファイルを含める場合
+              Dir.entries('.').sort # ディレクトリ内の全てのファイルと隠しファイル（. で始まるもの）を取得。その後、sort メソッドを使ってアルファベット順にソートする
+            else # 隠しファイルを除外する場合
+              Dir.glob('*') # 隠しファイルを除いたファイルを取得。
+            end
+    files.reverse! if @reverse # もし @reverse が true であれば、files.reverse! でファイルリストを逆順に並べ替える。
+    files
+  end
 end
