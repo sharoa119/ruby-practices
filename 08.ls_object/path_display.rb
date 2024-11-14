a60a5bead6cb92bc9fe5b9bcbbf77f9d24d69a70
sharoa119 <sau.rin1109@gmail.com>
2024-11-14 12:15:45 +0900

    ファイルを列形式で表示

--- 08.ls_object/path_display.rb
+++ 08.ls_object/path_display.rb
@@ -1,4 +1,26 @@
 # frozen_string_literal: true
 
 class PathDisplay
+  COLUMN_PADDING = 10
+
+  def initialize(files, column: 3)
+    @files = files
+    @column = column
+  end
+
+  def show
+    max_name_length = display_column_files.map(&:length).max
+    display_column_files.each_with_index do |file, index|
+      print file.ljust(max_name_length + COLUMN_PADDING)
+      puts if (index % @column).zero?
+    end
+  end
+
+  private
+
+  def display_column_files
+    max_line = (@files.size / COLUMN.to_f).ceil
+    column_files = @files.each_slice(max_line).to_a
+    column_files.transpose.flatten
+  end
 end
