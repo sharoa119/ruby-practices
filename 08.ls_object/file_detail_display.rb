04c7181293770cf2565c83dafa6d8a9c03a875e8
sharoa119 <sau.rin1109@gmail.com>
2024-11-14 12:16:59 +0900

    詳細情報を表示。変更箇所はまだありそう。途中

--- 08.ls_object/file_detail_display.rb
+++ 08.ls_object/file_detail_display.rb
@@ -1,4 +1,35 @@
 # frozen_string_literal: true
 
 class FileDetailDisplay
+  def initialize(files)
+    @files = files
+  end
+
+  def show
+    long_formats = @files.map { |file| DetailedFile.new(file).attributes }
+    max_size = get_max_size(long_formats)
+    total_blocks = long_formats.map { |format| format[:blocks] }.sum
+    puts "total #{total_blocks}"
+    long_formats.each do |format|
+      print "#{format[:type]}#{format[:mode]} "
+      print "#{format[:nlink].rjust(max_size[:nlink])} "
+      print "#{format[:username].ljust(max_size[:username])}  "
+      print "#{format[:groupname].ljust(max_size[:groupname])}  "
+      print "#{format[:bitesize].rjust(max_size[:bitesize])} "
+      print "#{format[:mtime]} "
+      print "#{format[:pathname]}\n"
+    end
+  end
+
+  private
+
+  def get_max_size(long_formats)
+    {
+      nlink: long_formats.map { |format| format[:nlink].size }.max,
+      username: long_formats.map { |format| format[:username].size }.max,
+      groupname: long_formats.map { |format| format[:groupname].size }.max,
+      bitesize: long_formats.map { |format| format[:bitesize].size }.max
+    }
+  end
 end
+
