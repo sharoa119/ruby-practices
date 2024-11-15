61a666b24448759d11ece29f9400c7e158a28a74
sharoa119 <sau.rin1109@gmail.com>
2024-11-15 10:39:52 +0900

    ファイルの列表示を変更。

--- 08.ls_object/path_display.rb
+++ 08.ls_object/path_display.rb
@@ -8,19 +8,29 @@ class PathDisplay
     @column = column
   end
 
+  # def show
+  #   max_name_length = display_column_files.map(&:length).max
+  #   display_column_files.each_with_index do |file, index|
+  #     print file.ljust(max_name_length + COLUMN_PADDING)
+  #     puts if (index % @column).zero?
+  #   end
+  # end
+
   def show
-    max_name_length = display_column_files.map(&:length).max
-    display_column_files.each_with_index do |file, index|
-      print file.ljust(max_name_length + COLUMN_PADDING)
-      puts if (index % @column).zero?
+    max_name_length = @files.map(&:length).max
+    padded_files = @files.map { |file| file.ljust(max_name_length + COLUMN_PADDING) }
+
+    # 各行にcolumn列ずつ表示
+    padded_files.each_slice(@column) do |line|
+      puts line.join
     end
   end
 
-  private
+  # private
 
-  def display_column_files
-    max_line = (@files.size / @column.to_f).ceil
-    column_files = @files.each_slice(max_line).to_a
-    column_files.transpose.flatten
-  end
+  # def display_column_files
+  #   max_line = (@files.size / @column.to_f).ceil
+  #   column_files = @files.each_slice(max_line).to_a
+  #   column_files.transpose.flatten
+  # end
 end
